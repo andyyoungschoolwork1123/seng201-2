02a5b0868ec38dfcebfd36f96b28cf5c6a8b5301
YYT1230 <126909366+YYT1230@users.noreply.github.com>
2023-05-08 19:40:26 +1200

    added PlayertestTwo
    added

---
+++ src/PlayerTestTwo.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+public class PlayerTestTwo {
+    public static void main(String[] args) {
+        Player player = new Player("Easy");
+
+        // get difficulty
+        System.out.println("Difficulty: " + player.getDifficulty());
+
+        // get gold
+        System.out.println("Starting gold: " + player.getGold());
+
+        // set and get team
+        ArrayList<Athlete> team = new ArrayList<Athlete>();
+        Athlete athlete1 = new Athlete("John Doe", 100, 70, 60, "Forward", 200, 100, 1);
+        Athlete athlete2 = new Athlete("Mike", 70, 50, 40, "Midfielder");
+        player.setTeam(team);
+        System.out.println("Team: " + player.getTeam());
+
+        // set and get points
+        player.increasepoints(10);
+        System.out.println("Points: " + player.getpoints());
+
+        // add and get inventory
+        ArrayList<EffectItem> inventory = new ArrayList<EffectItem>();
+        inventory.add(new EffectItem("Super Drink", "Consumable", 10, 5, 2));
+        inventory.add(new EffectItem("Revive", "Consumbale", 12, 4, 3));
+        player.setInventory(inventory);
+        System.out.println("Inventory: ");
+        player.getInventory();
+
+        // get team names
+        System.out.println("Team Names: ");
+        player.getTeamanmes();
+    }
+}
+
