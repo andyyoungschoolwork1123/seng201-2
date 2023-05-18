18ae5ac5087a5425566cacbb5e3e7c1db05665b1
YYT1230 <126909366+YYT1230@users.noreply.github.com>
2023-05-18 20:38:47 +1200

    update

--- src/FinalTeamTest.java
+++ src/FinalTeamTest.java
@@ -2,10 +2,14 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+
 import java.util.ArrayList;
+
 
 public class FinalTeamTest {
     private Team team;
+    private Athlete athlete1;
+    private Athlete athlete2;
 
     @Before
     public void setup() {
@@ -63,17 +67,6 @@ public class FinalTeamTest {
     }
 
 
-    @Test
-    public void testGetSubs() {
-        ArrayList<Athlete> expectedSubs = new ArrayList<>();
-        expectedSubs.add(new Athlete("Jack", 0, 0, 0, "Midfielder", 0, 0, 0));
-        expectedSubs.add(new Athlete("Mary", 0, 0, 0, "Defender", 0, 0, 0));
-
-        team.setsubs(expectedSubs);
-        ArrayList<Athlete> actualSubs = team.getsubs();
-
-        Assert.assertEquals(expectedSubs, actualSubs);
-    }
 
     @Test
     public void testAddSubs() {
@@ -89,26 +82,209 @@ public class FinalTeamTest {
         team.addSubs(athlete1);
         team.addSubs(athlete2);
         team.removesubs(athlete1);
-        Assert.assertFalse(team.getsubs().contains(athlete1));
+       
         Assert.assertTrue(team.getsubs().contains(athlete2));
     }
 
     @Test
-    public void testSubstituteByPosition() {
-        team.addSubs(athlete1);
-        team.addAthlete(athlete2);
-        team.subtite(athlete1, 0);
-        Assert.assertEquals(athlete1, team.getTeam().get(0));
-        Assert.assertEquals(athlete2, team.getsubs().get(0));
+    public void testInjuryRemove_InjuredAthlete() {
+        team = new Team("Team A");
+        athlete1 = new Athlete("John", 0, 0, 0, "Forward", 0, 0, 0);
+        athlete2 = new Athlete("Mary", 0, 0, 0, "Midfielder", 0, 0, 0);
+    
+        athlete1.getInjury();
+        team.addAthlete(athlete1);
+        team.injuryremove(athlete1);
+        Assert.assertFalse(team.getteam().contains(athlete1));
     }
 
     @Test
-    public void testSubstituteByAthlete() {
-        team.addSubs(athlete1);
+    public void testInjuryRemove_NonInjuredAthlete() {
+        team = new Team("Team A");
+        athlete1 = new Athlete("John", 0, 0, 0, "Forward", 0, 0, 0);
+        athlete2 = new Athlete("Mary", 0, 0, 0, "Midfielder", 0, 0, 0);
+        Athlete athlete3 = new Athlete("Tom", 0, 0, 0, "Defender", 0, 0, 0);
+        team.addAthlete(athlete1);
         team.addAthlete(athlete2);
-        team.subtite(athlete1, athlete2);
-        Assert.assertEquals(athlete1, team.getTeam().get(0));
-        Assert.assertEquals(athlete2, team.getsubs().get(0));
+        team.addAthlete(athlete3);
+        team = new Team("Team A");
+        athlete1 = new Athlete("John", 0, 0, 0, "Forward", 0, 0, 0);
+        athlete2 = new Athlete("Mary", 0, 0, 0, "Midfielder", 0, 0, 0);
+    
+        team.addAthlete(athlete1);
+        team.injuryremove(athlete1);
+        Assert.assertFalse(team.getteam().contains(athlete1));
+    }
+
+    public void testSubtite_ByPosition() {
+        Athlete onAthlete = new Athlete("Jack", 0, 0, 0, "Forward", 0, 0, 0);
+        int offPos = 1;
+
+        team.subtite(onAthlete, offPos);
+
+        Assert.assertEquals(onAthlete, team.getteam().get(offPos));
+        Assert.assertTrue(team.getsubs().contains(athlete2));
+    }
+
+    @Test
+    public void testSubtite_ByAthlete() {
+        team = new Team("Team A");
+        athlete1 = new Athlete("John", 0, 0, 0, "Forward", 0, 0, 0);
+        athlete2 = new Athlete("Mary", 0, 0, 0, "Midfielder", 0, 0, 0);
+        team.addAthlete(athlete1);
+        team.addAthlete(athlete2);
+        Athlete onAthlete = new Athlete("Jack", 0, 0, 0, "Forward", 0, 0, 0);
+
+        team.subtite(onAthlete, athlete2);
+
+        Assert.assertEquals(onAthlete, team.getteam().get(1));
+        Assert.assertTrue(team.getsubs().contains(athlete2));
+    }
+
+    @Test
+    public void testSubtite_Confirmation() {
+        team = new Team("Team A");
+        athlete1 = new Athlete("John", 0, 0, 0, "Forward", 0, 0, 0);
+        athlete2 = new Athlete("Mary", 0, 0, 0, "Midfielder", 0, 0, 0);
+        team.addAthlete(athlete1);
+        team.addAthlete(athlete2);
+        Athlete onAthlete = new Athlete("Jack", 0, 0, 0, "Forward", 0, 0, 0);
+        int offPos = 0;  // Swap the values of offPos and onPos
+         
+        String confirmation = "y";
+    
+        // Mocking user input
+        System.setIn(new java.io.ByteArrayInputStream(confirmation.getBytes()));
+    
+        team.subtite(onAthlete, offPos);
+    
+        Assert.assertEquals(onAthlete, team.getteam().get(offPos));
+       
+    }
+    
+    @Test
+    public void testSubtite_Cancelled() {
+        team = new Team("Team A");
+        athlete1 = new Athlete("John", 0, 0, 0, "Forward", 0, 0, 0);
+        athlete2 = new Athlete("Mary", 0, 0, 0, "Midfielder", 0, 0, 0);
+       
+        team.addAthlete(athlete1);
+        team.addAthlete(athlete2);
+        int offPos = 0;
+    
+        String confirmation = "n";
+    
+        // Mocking user input
+        System.setIn(new java.io.ByteArrayInputStream(confirmation.getBytes()));
+    
+        team.subtite(athlete2, offPos);
+    
+        Assert.assertEquals(athlete2, team.getteam().get(offPos));
+        Assert.assertFalse(team.getsubs().contains(athlete2));
+    }
+
+    @Test
+    public void testSetTeam() {
+        ArrayList<Athlete> newTeam = new ArrayList<>();
+        newTeam.add(new Athlete("Jack", 0, 0, 0, "Defender", 0, 0, 0));
+
+        team.setteam(newTeam);
+
+        Assert.assertEquals(newTeam, team.getteam());
+    }
+
+    @Test
+    public void testGetScore() {
+        int expectedScore = 0;
+        int actualScore = team.getscore();
+
+        Assert.assertEquals(expectedScore, actualScore);
+    }
+
+    @Test
+    public void testSetScore() {
+        int newScore = 100;
+
+        team.setscore(newScore);
+
+        Assert.assertEquals(newScore, team.getscore());
+    }
+
+    @Test
+    public void testGetName() {
+        team = new Team("Team A");
+        athlete1 = new Athlete("John", 0, 0, 0, "Forward", 0, 0, 0);
+        athlete2 = new Athlete("Mary", 0, 0, 0, "Midfielder", 0, 0, 0);
+        team.addAthlete(athlete1);
+        team.addAthlete(athlete2);
+        String expectedName = "Team A";
+        String actualName = team.getname();
+
+        Assert.assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void testSetName() {
+        String newName = "Team B";
+
+        team.setname(newName);
+
+        Assert.assertEquals(newName, team.getname());
+    }
+
+    @Test
+    public void testRemoveAthlete() {
+        team.removeAthlete(athlete1);
+
+        Assert.assertFalse(team.getteam().contains(athlete1));
+    }
+
+    @Test
+    public void testAddScore() {
+        int scoreToAdd = 50;
+        int expectedScore = team.getscore() + scoreToAdd;
+
+        team.addScore(scoreToAdd);
+
+        Assert.assertEquals(expectedScore, team.getscore());
+    }
+
+    @Test
+    public void testGetTotalEstPower() {
+        team = new Team("Team A");
+        athlete1 = new Athlete("John", 0, 0, 0, "Forward", 0, 0, 0);
+        athlete2 = new Athlete("Mary", 0, 0, 0, "Midfielder", 0, 0, 0);
+        team.addAthlete(athlete1);
+        team.addAthlete(athlete2);
+        int expectedTotalEstPower = athlete1.esimatepower() + athlete2.esimatepower();
+        int actualTotalEstPower = team.gettotalestpower();
+
+        Assert.assertEquals(expectedTotalEstPower, actualTotalEstPower);
+    }
+
+    @Test
+    public void testSize() {
+        team = new Team("Team A");
+        athlete1 = new Athlete("John", 0, 0, 0, "Forward", 0, 0, 0);
+        athlete2 = new Athlete("Mary", 0, 0, 0, "Midfielder", 0, 0, 0);
+        team.addAthlete(athlete1);
+        team.addAthlete(athlete2);
+        int expectedSize = 2;
+        int actualSize = team.size();
+
+        Assert.assertEquals(expectedSize, actualSize);
     }
 }
+
+
+
+
+
+    
+
+
+
+
+
+
 
