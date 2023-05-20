be82f67e6c0f9d9e0434529c0f01926a6c3f24ee
andyyoungschoolwork1123 <hcy11@uclive.ac.nz>
2023-05-20 16:19:35 +1200

    Arena 0.5

--- src/ArenaGUI.java
+++ src/ArenaGUI.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 public class ArenaGUI {
 
     private Arena arena;
@@ -10,7 +12,7 @@ public class ArenaGUI {
     private JComboBox<Team> opponentSelect; 
     private JButton subButton;
     private JButton startBattleButton;
-
+    private JFrame frame;
     public ArenaGUI(Player player) {
         this.arena =new Arena();
         this.player = player;
@@ -20,13 +22,22 @@ public class ArenaGUI {
     public void createAndShowGUI() {
         JFrame frame = new JFrame("Arena Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1000, 600);
 
         JPanel panel = new JPanel();
         frame.add(panel);
         placeComponents(panel);
-
         frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowClosed(WindowEvent e) {
+            updateplayerTeams();
+            // your additional code here, e.g., reopening the GameGUI
+            GameGUI gameGUI = new GameGUI(player);
+            //...
+        }
+    });
     }
 
     private void placeComponents(JPanel panel) {
@@ -35,13 +46,32 @@ public class ArenaGUI {
         // Create the substitute player button
         subButton = new JButton("Substitute Player");
         subButton.setBounds(10, 10, 150, 25);
-        subButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                arena.subplayer(playerTeam);
-                // Update the UI to reflect the new player state
+        subButton.addActionListener(e -> {
+            if (playerTeam.getsubs().size() == 0) {
+                subButton.setEnabled(false);
             }
-        });
+            else{
+                subButton(frame);}
+            });
+            
+        
+        panel.add(subButton);
+
+        //hint
+        JLabel LBlhintsub = new JLabel("Hint: You can sub only before match start");
+        LBlhintsub.setBounds(550, 10, 300, 25);
+        panel.add(LBlhintsub);
+        JLabel LBLhintBattle = new JLabel("Hint: Battle against the opponent team");
+        LBLhintBattle.setBounds(550, 40, 300, 25);
+        panel.add(LBLhintBattle);
+        JLabel LBLhintBattle2 = new JLabel("Hint: esitmate strength of opponent team only work as a reference");
+        LBLhintBattle2.setBounds(550, 70, 400, 25);
+        panel.add(LBLhintBattle2);
+        JLabel LBLhintBattle3 = new JLabel("Hint: You can only battle against one team per turn");
+        LBLhintBattle3.setBounds(550, 100, 400, 25);
+        panel.add(LBLhintBattle3);
+
+        //text area for main battle
         panel.add(subButton);
         JTextArea textArea = new JTextArea();
         textArea.setBounds(10, 40, 500, 500);
@@ -52,12 +82,12 @@ public class ArenaGUI {
 
         // Create the opponent selection dropdown
         opponentSelect = new JComboBox<>();
-        opponentSelect.setBounds(170, 10, 150, 25);
+        opponentSelect.setBounds(170, 10, 200, 25);
         panel.add(opponentSelect);
 
         // Create the start battle button
         startBattleButton = new JButton("Start Battle");
-        startBattleButton.setBounds(330, 10, 150, 25);
+        startBattleButton.setBounds(380, 10, 150, 25);
         startBattleButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -72,8 +102,25 @@ public class ArenaGUI {
             }
         });
         panel.add(startBattleButton);
-    }
+        JButton btnUseitemButton = new JButton("Use Item from inventory");
+        btnUseitemButton.setBounds(550, 130, 200, 25);
+        btnUseitemButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //arena.battle(playerTeam,selectedOpponent,player);
+                //String battleResult = playerTeam.su(playerTeam, player); // call the function
+                //textArea.setText(battleResult); // set the text
 
+                // Update the UI to reflect the new game state
+            }
+        });
+    }
+    public void subButton(JFrame frame) {
+        SubstitutionDialog dialog = new SubstitutionDialog(frame, playerTeam);
+                    dialog.pack();
+                    dialog.setVisible(true);        // Update the UI to reflect the new player state
+
+    }
     public void updateOpponents() {
         // Clear the current items
         opponentSelect.removeAllItems();
@@ -84,14 +131,23 @@ public class ArenaGUI {
             opponentSelect.addItem(opponent);
         }
     }
+    public void updateplayerTeams(){
+        //update player result
+        player.setTeam(playerTeam.getteam());
+        player.setsubs(playerTeam.getsubs());
 
+    }
+    public Player getPlayer() {
+        updateplayerTeams();
+        return this.player;
+    }
     public static void main(String[] args) {
         Player player = new Player("Easy", "testing");
         Team team = new Team();
         player.setteam(team);
-
         ArenaGUI arenaGUI = new ArenaGUI(player);
         arenaGUI.createAndShowGUI();
         arenaGUI.updateOpponents();
     }
 }
+    
