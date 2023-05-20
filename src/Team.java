97f0c2f7bfd894a2fc6163e177e42b104ae61d32
YYT1230 <126909366+YYT1230@users.noreply.github.com>
2023-05-20 16:51:06 +1200

    Team javadoc
    Team Javadoc

--- src/Team.java
+++ src/Team.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
-
+/**
+ * Represents a team consisting of athletes.
+ */
 public class Team {
     public ArrayList<Athlete> team;
     private ArrayList<Athlete> subs = new ArrayList<Athlete>();
@@ -13,12 +15,20 @@ public class Team {
         this.score = team.score;
         this.subs = team.subs;
     }
+    /**
+     * Creates a new Team object by copying the properties of another Team object.
+     * @param team The Team object to copy.
+     */
     public Team(String name) {
         this.name = name;
         this.team = new ArrayList<Athlete>();
         this.score = 0;
         this.subs = new ArrayList<Athlete>();
     }
+    /**
+     * Creates a new Team object with the specified name.
+     * @param name The name of the team.
+     */
     public Team(String name, ArrayList<Athlete> team,ArrayList<Athlete> subs) {
         this.name = name;
         this.team = team;
@@ -26,6 +36,10 @@ public class Team {
         this.subs = subs;
 
     }
+
+     /**
+     * Creates a default Team object with default athletes.
+     */
     public Team(){
         this.name = "default";
         this.team = new ArrayList<Athlete>();
@@ -37,6 +51,12 @@ public class Team {
         this.team.add(new Athlete("d", 100, 100, 100, "default"));
         this.team.add(new Athlete("e", 100, 100, 100, "default"));
     }
+
+    /**
+     * Creates a Team object with the specified name and team.
+     * @param name The name of the team.
+     * @param team The list of athletes in the team.
+     */
     public Team(String name, ArrayList<Athlete> team) {
         this.name = name;
         this.team = team;
@@ -44,6 +64,10 @@ public class Team {
         this.subs = new ArrayList<Athlete>();
 
     }
+    /**
+     * Creates a Team object based on the specified Player's information.
+     * @param player The player object from which to create the team.
+     */
     public Team(Player player) {
         this.name = player.getName();
         this.team = player.getTeam();
@@ -51,23 +75,44 @@ public class Team {
         this.subs = player.getsubs();
 
     }
+  /**
+     * Returns the current position of the team.
+     * @return The position of the team.
+     */
     public int getpos(){
         return this.pos;
     }
+/**
+     * Sets the position of the team.
+     * @param pos The position to set.
+     */
     public void setpos(int pos){
         this.pos = pos;
     }
+     /**
+     * Returns the substitutes list.
+     * If the list is null, it initializes an empty list and returns it.
+     * @return The substitutes list.
+     */
     public ArrayList<Athlete> getsubs(){
         if (this.subs == null){
             this.subs = new ArrayList<Athlete>();
         }
         return this.subs;
     }
-    
+    /**
+     * Sets the substitutes list.
+     * @param subs The substitutes list to set.
+     */
+
     public void setsubs(ArrayList<Athlete> subs){
         this.subs = subs;
     }
-    
+    /**
+     * Adds an athlete to the team.
+     * If the maximum team size has been reached, it displays a message.
+     * @param athlete The athlete to add.
+     */
     public void addAthlete(Athlete athlete) {
         if (team.size() < maxSize) {
             team.add(athlete);
@@ -75,7 +120,11 @@ public class Team {
             System.out.println("Maximum team size reached.");
         }
     }
-
+  /**
+     * Adds an athlete to the substitutes list.
+     * If the maximum substitutes size has been reached, it displays a message.
+     * @param athlete The athlete to add.
+     */
     public void addSubs(Athlete athlete) {
         if (subs.size() < maxSize) {
             subs.add(athlete);
@@ -83,31 +132,58 @@ public class Team {
             System.out.println("Maximum subs size reached.");
         }
     }
+    /**
+     * Removes an athlete from the substitutes list.
+     * @param athlete The athlete to remove.
+     */
     public void removesubs(Athlete athlete){
         this.subs.remove(athlete);
     }
+    /**
+     * Removes an athlete from the team if they are injured.
+     * @param athlete The athlete to check and remove.
+     */
     public void injuryremove(Athlete athlete){
         if (athlete.getInjury() == true){
             this.team.remove(athlete);
         }
         this.team.remove(athlete);
     }
+    /**
+     * Prints the list of substitutes.
+     */
+
     public void printsubs(){
         for (int i = 0; i < this.subs.size(); i++) {
             Athlete athlete = this.subs.get(i);
             System.out.println((i+1) + ". " + athlete.getName() + " (" + athlete.getRole() + ")");
         }
     }
+    /**
+     * Substitutes an athlete on the team with an athlete from the substitutes list.
+     * @param onathlete The athlete to substitute on.
+     * @param off_pos The position of the athlete to substitute off.
+     */
     public void subtite(Athlete onathlete, int off_pos){
         //gui in mind
         this.subs.add(this.team.get(off_pos));
         this.team.set(off_pos, onathlete);
     }
+     /**
+     * Substitutes an athlete on the team with a specific athlete from the substitutes list.
+     * @param onathlete The athlete to substitute on.
+     * @param offathlete The athlete to substitute off.
+     */
     public void subtite(Athlete onathlete, Athlete offathlete){
         //gui in mind
         this.subs.add(offathlete);
         this.team.set(this.team.indexOf(offathlete), onathlete);
     }
+    /**
+     * Substitutes an athlete on the team with an athlete from the substitutes list based on their positions.
+     * @param on_pos The position of the athlete to substitute on from the substitutes list.
+     * @param off_pos The position of the athlete to substitute off from the team.
+     */
     public void subtite(int on_pos, int off_pos){
         //grouping with the select_subon() and select_suboff()
         System.out.println("Substituting " + this.team.get(off_pos).getName() + " with " + this.subs.get(on_pos).getName() + "comfirm? (y/n)");
@@ -121,50 +197,97 @@ public class Team {
         }   
         
     }
+    /**
+     * Returns the list of athletes in the team.
+     * @return The list of athletes.
+     */
     
     public ArrayList<Athlete> getteam(){
         return this.team;
     }
+    /**
+     * Prints the list of athletes in the team.
+     */
     public void printteam(){
         for (int i = 0; i < this.team.size(); i++) {
             Athlete athlete = this.team.get(i);
             System.out.println((i+1) + ". " + athlete.getName() + " (" + athlete.getRole() + ")");
         }
     }
+    /**
+ * Allows the user to select the athlete they want to substitute on from the substitutes list.
+ * @return The position of the selected athlete in the substitutes list.
+ */
     public int select_subon(){
         System.out.println("Select the athlete you want to sub on:");
         this.printsubs();
         int subon_pos = Integer.parseInt(System.console().readLine());
         return subon_pos;
     }
+    /**
+ * Allows the user to select the athlete they want to substitute off from the team.
+ * @return The position of the selected athlete in the team.
+ */
     public int select_suboff(){
         System.out.println("Select the athlete you want to sub off:");
         this.printteam();
         int suboff_pos = Integer.parseInt(System.console().readLine());
         return suboff_pos;
     }
+    /**
+ * Sets the team with the specified list of athletes.
+ * @param team The list of athletes to set as the team.
+ */
     public void setteam(ArrayList<Athlete> team){
         this.team = team;
     }
+    /**
+ * Returns the current score of the team.
+ * @return The score of the team.
+ */
     public int getscore(){
         return this.score;
     }
+    /**
+ * Sets the score of the team.
+ * @param score The score to set.
+ */
     public void setscore(int score){
         this.score = score;
     }
+    /**
+ * Returns the name of the team.
+ * @return The name of the team.
+ */
     public String getname(){
         return this.name;
     }
+    /**
+ * Sets the name of the team.
+ * @param name The name to set.
+ */
     public void setname(String name){
         this.name = name;
     }
-    
+    /**
+ * Removes the specified athlete from the team.
+ * @param athlete The athlete to remove.
+ */
     public void removeAthlete(Athlete athlete) {
         this.team.remove(athlete);
     }
+    /**
+ * Adds the specified score to the team's current score.
+ * @param score The score to add.
+ */
     public void addScore(int score) {
         this.score += score;
     }
+
+/**
+ * Calculates and returns the total estimated power of the team.
+ * @return The total estimated power of the team.
+ */
     public int gettotalestpower(){
         int total = 0;
         for (int i = 0; i < this.team.size(); i++) {
@@ -173,6 +296,9 @@ public class Team {
         }
         return total;
     }
+/**
+ * Displays the team information in the command-line interface.
+ */
     public void displayTeam_cmd() {
         System.out.println("Team " + this.name + ":");
         for (int i = 0; i < this.team.size(); i++) {
@@ -185,6 +311,10 @@ public class Team {
 
         }
     }
+/**
+ * Returns the number of athletes in the team.
+ * @return The number of athletes in the team.
+ */
     public int size() {
         return this.team.size();
     }
