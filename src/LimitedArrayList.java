6e871eb71232e44cf123d9cd40ad601ac56aa6f6
YYT1230 <126909366+YYT1230@users.noreply.github.com>
2023-05-22 21:38:24 +1200

    LimitedArrayList javadoc
    LimitedArrayList javadoc

--- src/LimitedArrayList.java
+++ src/LimitedArrayList.java
@@ -1,12 +1,26 @@
 import java.util.ArrayList;
 
 public class LimitedArrayList<E> extends ArrayList<E> {
+/**
+ * The LimitedArrayList class is a custom implementation of ArrayList that imposes a maximum size limit on the list.
+ *
+ * @param <E> the type of elements in the list
+ */
     private final int maxSize;
-
+    /**
+     * Constructs a new LimitedArrayList with the specified maximum size.
+     *
+     * @param maxSize the maximum size of the list
+     */
     public LimitedArrayList(int maxSize) {
         this.maxSize = maxSize;
     }
-
+    /**
+     * Adds the specified element to the list if the maximum size has not been reached.
+     *
+     * @param element the element to be added
+     * @return true if the element was added successfully, false otherwise
+     */
     @Override
     public boolean add(E element) {
         if (size() < maxSize) {
