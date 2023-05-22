8fa1018fe1cc3dab24d530a1995daf484e8c6077
YYT1230 <126909366+YYT1230@users.noreply.github.com>
2023-05-22 21:18:48 +1200

    AthleteListCellRenderer javadoc
    AthleteListCellRenderer javadoc

--- src/AthleteListCellRenderer.java
+++ src/AthleteListCellRenderer.java
@@ -1,8 +1,22 @@
 import java.awt.Component;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.JList;
+/**
+ * A custom list cell renderer for displaying Athlete objects in a JList.
+ * This renderer sets the text of each list cell to the athlete's name.
+ */
 class AthleteListCellRenderer extends DefaultListCellRenderer {
     @Override
+    /**
+     * Returns the component used for rendering the list cell.
+     *
+     * @param list         the JList being painted
+     * @param value        the value to be rendered
+     * @param index        the cell index
+     * @param isSelected  true if the cell is selected
+     * @param cellHasFocus true if the cell has focus
+     * @return the component used for rendering the list cell
+     */
     public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
         super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
         if (value instanceof Athlete) {
