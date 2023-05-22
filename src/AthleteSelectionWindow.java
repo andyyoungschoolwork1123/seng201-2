704b8e936b418d59802b518f46156c037cd06e1f
andyyoungschoolwork1123 <hcy11@uclive.ac.nz>
2023-05-22 22:38:04 +1200

    Update AthleteSelectionWindow.java
    refactored the codes,following java pratices

--- src/AthleteSelectionWindow.java
+++ src/AthleteSelectionWindow.java
@@ -1,114 +1,121 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
-
 /**
  * Represents a window for selecting athletes.
  */
 
 public class AthleteSelectionWindow extends JFrame {
 
+    private static final int WINDOW_WIDTH = 720;
+    private static final int WINDOW_HEIGHT = 600;
+
     private Player player;
-    private ArrayList<Athlete> team = new ArrayList<Athlete>();
+    private ArrayList<Athlete> team = new ArrayList<>();
+    private JButton[] athleteButtons;
     /**
      * Creates an instance of AthleteSelectionWindow.
      *
      * @param player the player object
      */
-
     public AthleteSelectionWindow(Player player) {
         this.player = player;
-        initialize();
+        initializeWindow();
+        initializeComponents();
     }
     /**
-     * Initializes the athlete selection window.
+     * Initializes the window settings.
      */
-
-    private void initialize() {
+    private void initializeWindow() {
         setTitle("Athlete Selection");
-        setBounds(100, 100, 720, 600);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().setLayout(null);
-
-        JPanel athletePanel = new JPanel();
-        athletePanel.setBounds(50, 50, 750, 450);
-        getContentPane().add(athletePanel);
-        athletePanel.setLayout(null);
-
-        JLabel lblAthleteSelection = new JLabel("Select 5 athletes:");
-        lblAthleteSelection.setBounds(10, 10, 150, 20);
-        athletePanel.add(lblAthleteSelection);
-
-        JButton[] athleteButtons = new JButton[10];
-        ArrayList<Athlete> listAthlete = player.init_team();
-        for (int i = 0; i < 10; i++) {
-            Athlete athleteOnSelect = listAthlete.get(i);
-
-            JButton athleteButton = new JButton("Select " + athleteOnSelect.getName());
-            athleteButton.setBounds(10 + (i % 2) * 160, 40 + (i / 2) * 50, 150, 30);
-            athleteButton.setToolTipText(athleteOnSelect.getName() + " (" + athleteOnSelect.getRole() + ")\n"
-                    + "Stamina: " + athleteOnSelect.getStamina() + "\n"
-                    + "Offense: " + athleteOnSelect.getOffence() + "\n"
-                    + "Defense: " + athleteOnSelect.getDefence());
-            athletePanel.add(athleteButton);
-
-            athleteButton.addActionListener(e -> {
-                if (athleteButton.getText().startsWith("Select")) {
-                    athleteButton.setText("Deselect " + athleteOnSelect.getName());
-                    team.add(athleteOnSelect);
-                } else {
-                    athleteButton.setText("Select " + athleteOnSelect.getName());
-                    team.remove(athleteOnSelect);
-                }
-            });
-
-            athleteButtons[i] = athleteButton; // Assign the button to the array
-        }
-
-        JButton viewAthletesButton = new JButton("View Selected Athletes");
-        viewAthletesButton.setBounds(450, 500, 160, 30);
-        getContentPane().add(viewAthletesButton);
-        viewAthletesButton.addActionListener(e -> openSelectedAthletesWindow());
-        JButton checkButton = new JButton("Check Selection");
-        checkButton.setBounds(250, 500, 160, 30);
-        getContentPane().add(checkButton);
-        checkButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (team.size() == 5) {
-                    proceedToNextWindow();
-                } else {
-                    JOptionPane.showMessageDialog(AthleteSelectionWindow.this, "Please select exactly 5 athletes.", "Selection Error", JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        });
-        
-        setVisible(true);
-        
     }
     /**
-     * Opens the window to view the selected athletes.
+     * Initializes the components of the window.
+     */
+    private void initializeComponents() {
+        JPanel athletePanel = new JPanel();
+        athletePanel.setLayout(new GridLayout(5, 2));
+        add(athletePanel, BorderLayout.CENTER);
+        // Add a label for the athlete selection
+        JLabel lblAthleteSelection = new JLabel("Select 5 athletes to form your initial team:", SwingConstants.CENTER);
+        add(lblAthleteSelection, BorderLayout.NORTH);
+        // Add a button for each athlete
+        ArrayList<Athlete> listAthlete = player.init_team();
+        athleteButtons = new JButton[listAthlete.size()];
+        for (int i = 0; i < listAthlete.size(); i++) {
+            Athlete athlete = listAthlete.get(i);
+            JButton athleteButton = createAthleteButton(athlete);
+            athletePanel.add(athleteButton);
+            athleteButtons[i] = athleteButton;
+        }
+        // Add a panel for the buttons at the bottom
+        JPanel southPanel = new JPanel();
+        JButton viewAthletesButton = new JButton("View Selected Athletes");
+        viewAthletesButton.addActionListener(e -> openSelectedAthletesWindow());
+        southPanel.add(viewAthletesButton);
+        // Add a button for checking the selection
+        JButton checkButton = new JButton("Check Selection");
+        checkButton.addActionListener(e -> checkSelection());
+        southPanel.add(checkButton);
+
+        add(southPanel, BorderLayout.SOUTH);
+    }
+    /*
+     * Creates a button for selecting an athlete. 
+     * @param athlete the athlete to be selected
+     * @return the button for selecting the athlete
+     */
+    private JButton createAthleteButton(Athlete athlete) {
+        
+        JButton athleteButton = new JButton("Select " + athlete.getName());
+        athleteButton.setToolTipText(athlete.getName() + " (" + athlete.getRole() + ")\n"
+                + "Stamina: " + athlete.getStamina() + "\n"
+                + "Offense: " + athlete.getOffence() + "\n"
+                + "Defense: " + athlete.getDefence());
+        athleteButton.addActionListener(e -> toggleSelection(athleteButton, athlete));
+        return athleteButton;
+    }
+    /*
+     * Toggles the selection of an athlete.
+     * @param athleteButton the button for selecting the athlete
+     * @param  the athlete to be selected
+     */
+    private void toggleSelection(JButton athleteButton, Athlete athlete) {
+        if (athleteButton.getText().startsWith("Select")) {
+            athleteButton.setText("Deselect " + athlete.getName());
+            team.add(athlete);
+        } else {
+            athleteButton.setText("Select " + athlete.getName());
+            team.remove(athlete);
+        }
+    }
+    /*
+     * Opens a window to display the selected athletes.
      */
     private void openSelectedAthletesWindow() {
         SelectedAthletesWindow selectedAthletesWindow = new SelectedAthletesWindow(team);
         selectedAthletesWindow.setVisible(true);
     }
-    
-    /**
-     * Proceeds to the next window after athlete selection.
-     */                                                                              
+    /*
+     * Checks if the player has selected exactly 5 athletes.
+     */
+    private void checkSelection() {
+        if (team.size() == 5) {
+            proceedToNextWindow();
+        } else {
+            JOptionPane.showMessageDialog(this, "Please select exactly 5 athletes.", "Selection Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    /*
+     * Proceeds to the next window.
+     */
     private void proceedToNextWindow() {
         player.setTeam(team);
-
-        // Implement code to open the next window
         GameGUI gameGUI = new GameGUI(player);
-        this.dispose();
-
+        dispose();
     }
 }
+
 
